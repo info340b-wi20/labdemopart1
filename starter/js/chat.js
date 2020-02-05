00172797d0415438c881c6ee6ec2e322720624dd
Jonathan Cady <joncady@uw.edu>
2020-02-04 20:16:55 -0800

    fixed issues

--- starter/js/chat.js
+++ starter/js/chat.js
@@ -35,6 +35,10 @@ const chats = [
  * @param {list of chats to render} chats 
  */
 function renderChats(chats) {
+    let chatArea = document.querySelector("#chat");
+    chats.forEach(function (chat) {
+        chatArea.appendChild(renderOneChat(chat));
+    });
 }
 
 /**
@@ -42,6 +46,29 @@ function renderChats(chats) {
  * @param {object that represents a chat} chatObj 
  */
 function renderOneChat(chatObj) {
+    // create outer li
+    let li = document.createElement("li");
+    li.classList.add("chat", "list-group-item");
+    // create div w/ img
+    let imgDiv = document.createElement("div");
+    let img = document.createElement("img");
+    img.classList.add("chat-pic");
+    img.src = chatObj.pic;
+    img.alt = chatObj.name;
+    imgDiv.appendChild(img);
+    li.appendChild(imgDiv);
+    // create chat content
+    let chatDiv = document.createElement("div");
+    chatDiv.classList.add("chat-content");
+    let h4 = document.createElement("h4");
+    h4.innerHTML = chatObj.name + (chatObj.unread ? "<span class='unread'/>" : "");
+    chatDiv.appendChild(h4);
+    let chatMessage = document.createElement("div");
+    chatMessage.innerHTML = (chatObj.from ? "<strong>You:</strong> " : "") + chatObj.lastMessage;
+    chatDiv.appendChild(chatMessage);
+    li.appendChild(chatDiv);
+    return li;
 }
 
 // call the function to render chats
+renderChats(chats);

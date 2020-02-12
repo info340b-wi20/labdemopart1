ad98965efc9104ee7c61a12b44a740f70ac1f6b5
Jonathan Cady <joncady@uw.edu>
2020-02-12 11:44:53 -0800

    in class

--- starter/js/profile.js
+++ starter/js/profile.js
@@ -2,23 +2,68 @@ let state = {
     chosen: [],
     profileIndex: 0,
     profiles: []
-}
-
-function setProfile(profile) {
-}
-
-// gets the profiles...
-function fetchProfiles() {
+ }
+  
+ /**
+       {
+            "name": "Jane",
+            "age": 26,
+            "profession": "Cosplayer",
+            "bio": "I love the Mandalorian and Baby Yoda is so cute!",
+            "pic": "img/mando.jpg"
+        }
+ */
+  
+ function setProfile(profile) {
+    let img = document.querySelector(".card-img-top");
+    img.src = profile.pic;
+    img.alt = profile.name;
+    let bio = document.querySelector("#bio");
+    bio.textContent = profile.bio;
+    let name = document.querySelector("#title");
+    name.textContent = profile.name + ", " + profile.age;
+    let profession = document.querySelector("#profession");
+    profession.textContent = profile.profession;
+ }
+  
+ // gets the profiles...
+ function fetchProfiles() {
     console.log("Fetching...")
-}
-
-// sets up the listeners for the choose / pass
-function setUpListeners() {
-}
-
-// choose or pass...
-function chooseOrPass(which) {
-}
-
-fetchProfiles();
-setUpListeners();
+    fetch("js/data.json")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            state.profiles = data.profiles;
+            setProfile(state.profiles[state.profileIndex]);
+        });
+ }
+  
+ // sets up the listeners for the choose / pass
+ function setUpListeners() {
+    let red = document.querySelector(".circle.red");
+    let green = document.querySelector(".circle.green");
+    red.addEventListener("click", function() {
+        chooseOrPass(false);
+    });
+    green.addEventListener("click", function() {
+        chooseOrPass(true);
+    });
+ }
+  
+ // choose or pass...
+ function chooseOrPass(which) {
+    if (which) {
+        state.chosen.push(state.profiles[state.profileIndex]);
+    }
+    state.profileIndex++;
+    if (state.profileIndex < state.profiles.length) {
+        setProfile(state.profiles[state.profileIndex]);
+    } else {
+        document.querySelector(".card").style.display = "none";
+        console.log(state.chosen);
+    }
+ }
+  
+ fetchProfiles();
+ setUpListeners(); 

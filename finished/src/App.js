2f8ddd32857864ccc6298af0d7469a4a2f192510
Jonathan Cady <joncady@uw.edu>
2020-02-18 17:59:41 -0800

    set up for lab 6

---
+++ finished/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Nav from './Nav';
+import Main from './Main';
+import Footer from './Footer';
+import './App.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <Main />
+        <Footer />
+      </div>
+    );
+  }
+
+}
+
+export default App;
